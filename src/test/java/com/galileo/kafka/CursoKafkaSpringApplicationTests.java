d2aedadba2e4e52162c96b43464a66ada362ed92
Galileo Luna <galileoungs@gmail.com>
2021-04-12 14:53:37 -0300

    first commit

---
+++ src/test/java/com/galileo/kafka/CursoKafkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galileo.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoKafkaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
